3ad14266373448e9e307ffe6801af9bc67bb2260
pihotpiter <pihotpiter@gmail.com>
2026-07-19 21:24:41 +0300

    create_db.py

--- create_db.py
+++ create_db.py
@@ -1,19 +1,25 @@
 import sqlite3
 
-DB_NAME = "database.db"
-
-conn = sqlite3.connect(DB_NAME)
-
+conn = sqlite3.connect("database.db")
 cursor = conn.cursor()
 
 cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS users(
+
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
+
+    email TEXT UNIQUE NOT NULL,
+
+    username TEXT UNIQUE NOT NULL,
+
     password TEXT NOT NULL,
+
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+
 )
 """)
 
 conn.commit()
 conn.close()
+
+print("База данных создана.")
